75ea7407776fd47933fefe6497068bd96dc9a44a
unknown <acesoftwaretraining@gmail.com>
2025-03-21 18:26:01 +0530

    pointers

--- pointers.cpp
+++ pointers.cpp
@@ -4,11 +4,32 @@ using namespace std;
 
 int main(){
     string fruits[3] = {"apple", "orange", "banana"};
+    string *ptrP = fruits;
     string *ptr0 = &fruits[0];
     string *ptr1 = &fruits[1];
     string *ptr2 = &fruits[2];
-    cout << ptr0 << *ptr0 << endl;
-    cout << ptr1 << *ptr1 << endl;
-    cout << ptr2 << *ptr2 << endl;
+    // cout << ptrP << "\t" << sizeof(ptrP) << "\t" << *ptrP << endl;
+    // cout << ptr0 << "\t" << sizeof(ptr0) << "\t" << *ptr0 << endl;
+    // cout << ptr1 << "\t" << sizeof(ptr1) << "\t" << *ptr1 << endl;
+    // cout << ptr2 << "\t" << sizeof(ptr2) << "\t" << *ptr2 << endl;
+    delete ptrP;
+    delete ptr0;
+    delete ptr1;
+    delete ptr2;
+
+    // malloc calloc
+    string *malloc_ptr = (string*)malloc(5 * sizeof(string));
+    int *calloc_ptr = (int*)calloc(5, sizeof(int));
+
+    for (int i = 0; i < sizeof(malloc_ptr); i++)
+    {
+        cout << malloc_ptr[i] << endl;
+    }
+
+    for (int i = 0; i < sizeof(calloc_ptr); i++)
+    {
+        cout << calloc_ptr[i] << endl;
+    }
+    
     return 0;
 }
